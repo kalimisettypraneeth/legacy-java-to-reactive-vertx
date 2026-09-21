1a7ea776297c0dbb1b727bf7ae1b694466b5a0c1
SAI PRANEETH KALIMISETTY <83554932+kalimisettypraneeth@users.noreply.github.com>
2026-09-21 12:31:16 -0700

    Add blocking and reactive reference implementations

---
+++ implementation/legacy-monolith/src/main/java/research/legacy/LegacyMonolithApplication.java
@@ -0,0 +1,11 @@
+package research.legacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyMonolithApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LegacyMonolithApplication.class, args);
+    }
+}
